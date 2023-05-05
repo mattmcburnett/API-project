0e3f4f07295ec18d6a7b4c551bbd9176c94f2af6
Matthew McBurnett <mattmcburnett@gmail.com>
2023-05-05 14:21:15 -0700

    added user dropdown and nav component

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -48,6 +48,15 @@ export const signup = (user) => async (dispatch) => {
 };
 
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
+
 export const restoreUser = () => async (dispatch) => {
     const response = await csrfFetch("/api/session");
     const data = await response.json();
